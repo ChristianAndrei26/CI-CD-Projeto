eb9f4b7912621d99b1dad0cbe2927ba1043882ff
ChristianAndrei26 <fernandatvseries2017@gmail.com>
2026-09-12 10:40:01 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from feature.calculadora import soma
+import time
+
+
+print("Aplicação Calculadora iniciada com sucesso!")
+print("Teste da soma: 2 + 3 =", soma(2, 3))
+
+while True:
+    time.sleep(60)
